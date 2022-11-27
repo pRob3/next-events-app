429bf49f4cee28e057dfc7b7e5aa516bc37fd27b
pRob3 <skene@hotmail.com>
2022-11-27 19:21:46 +0100

    All events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,11 @@
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+
 function EventsPage() {
+  const events = getAllEvents();
   return (
     <div>
-      <h1>Events Page</h1>
+      <EventList items={events} />
     </div>
   );
 }
